94bbbe7d4f0d5cd23297d31b56cb4e0020db8332
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-13 11:58:33 +0330

    feat: add documentation code example component

---
+++ src/components/CodeExample.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="code-example">
+    <div class="code-header">{{ title }}</div>
+    <pre><code>{{ code }}</code></pre>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  code: String
+})
+</script>
